998f40ca54fe58995753a54ee37f3f1c3170fde5
Noah Mast <noahmast28@gmail.com>
2025-04-09 23:48:55 -0500

    Fixed types and included all libraries used in matrix.cpp

--- matrix.hpp
+++ matrix.hpp
@@ -2,9 +2,12 @@
 #define __MATRIX_HPP__
 
 #include <cstddef>
+#include <stdexcept>
+#include <iostream>
 
 // all of your function definitions should be in this file now
 
+template<typename T>
 class Matrix {
 private:
     T** data;
@@ -20,10 +23,10 @@ public:
     Matrix operator+(const Matrix &rhs) const;
     Matrix operator*(const Matrix &rhs) const;
     void set_value(std::size_t i, std::size_t j, int n);
-    int get_value(std::size_t i, std::size_t j) const;
-    int get_size() const;
-    int sum_diagonal_major() const;
-    int sum_diagonal_minor() const;
+    T get_value(std::size_t i, std::size_t j) const;
+    std::size_t get_size() const;
+    T sum_diagonal_major() const;
+    T sum_diagonal_minor() const;
     void swap_rows(std::size_t r1, std::size_t r2);
     void swap_cols(std::size_t c1, std::size_t c2);
     void print_matrix() const;
