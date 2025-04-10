e6f316f2dee3e83374e533a5cbd55b557d1a47ed
Noah Mast <noahmast28@gmail.com>
2025-04-09 23:12:10 -0500

    Edited functions to work with the template

--- matrix.cpp
+++ matrix.cpp
@@ -69,9 +69,9 @@ Matrix<T>& Matrix<T>::operator=(const Matrix& other) {
 
 
 // Returns the sum of two matrices
-Matrix Matrix::operator+(const Matrix &rhs) const {
-    std::size_t N = data.size(); // Gets the size of the matrices we are working with
-    Matrix result(N); // Creates a matrix of correct size to store the result
+template <typename T>
+Matrix<T> Matrix<T>::operator+(const Matrix &rhs) const {
+    Matrix result(size); // Creates a matrix of correct size to store the result
 
     for (std::size_t i = 0; i < N; i++) { // Iterates over the rows of the matrices
         for (std::size_t j = 0; j < N; j++) { // Iterates over the columns of the matrices
@@ -82,9 +82,9 @@ Matrix Matrix::operator+(const Matrix &rhs) const {
 }
 
 // Returns the dot product of two matrices
-Matrix Matrix::operator*(const Matrix &rhs) const {
-    std::size_t N = data.size(); // Gets the size of the matrices we are working with
-    Matrix result(N); // Creates a matrix of correct size to store the result
+template <typename T>
+Matrix<T> Matrix<T>::operator*(const Matrix &rhs) const {
+    Matrix result(size); // Creates a matrix of correct size to store the result
 
     for (std::size_t i = 0; i < N; i++) { // Iterates over the rows of this matrix
         for (std::size_t j = 0; j < N; j++) { // Iterates over the columns of the rhs matrix
@@ -98,7 +98,8 @@ Matrix Matrix::operator*(const Matrix &rhs) const {
 }
 
 // Sets a given value in a matrix
-void Matrix::set_value(std::size_t i, std::size_t j, int n) {
+template <typename T>
+void Matrix<T>::set_value(std::size_t i, std::size_t j, T n) {
     // Throws error if the index is out of range
     if (i >= data.size() || j >= data.size()) {
         throw std::out_of_range("Index out of range");
@@ -107,7 +108,8 @@ void Matrix::set_value(std::size_t i, std::size_t j, int n) {
 }
 
 // Returns a value from a given place in a matrix
-int Matrix::get_value(std::size_t i, std::size_t j) const {
+template <typename T>
+T Matrix<T>::get_value(std::size_t i, std::size_t j) const {
     // Throws error if the index is out of range
     if (i >= data.size() || j >= data.size()) {
         throw std::out_of_range("Index out of range");
@@ -116,13 +118,15 @@ int Matrix::get_value(std::size_t i, std::size_t j) const {
 }
 
 // Returns the size of the matrix
-int Matrix::get_size() const {
+template <typename T>
+int Matrix<T>::get_size() const {
     return data.size();
 }
 
 // Returns the sum of the major diagonal
-int Matrix::sum_diagonal_major() const {
-    int sum = 0; // Initializes the sum at 0
+template <typename T>
+T Matrix<T>::sum_diagonal_major() const {
+    T sum = 0; // Initializes the sum at 0 with type T
 
     for (std::size_t i = 0; i < data.size(); i++) { // Iterates over the size of the matrix
         sum += data[i][i]; // Adds each element along the major diagonal to the sum
@@ -132,8 +136,9 @@ int Matrix::sum_diagonal_major() const {
 }
 
 // Returns the sum of the minor diagonal
-int Matrix::sum_diagonal_minor() const {
-    int sum = 0; // Initializes the sum at 0
+template <typename T>
+T Matrix<T>::sum_diagonal_minor() const {
+    T sum = 0; // Initializes the sum at 0 with type T
 
     for (std::size_t i = 0; i < data.size(); i++) { // Iterates over the size of the matrix
         sum += data[i][data.size() - i - 1]; // Adds each element along the minor diagonal to the sum
@@ -143,14 +148,16 @@ int Matrix::sum_diagonal_minor() const {
 }
 
 // Swaps two rows in a matrix
-void Matrix::swap_rows(std::size_t r1, std::size_t r2) {
+template <typename T>
+void Matrix<T>::swap_rows(std::size_t r1, std::size_t r2) {
     if (r1 < data.size() && r2 < data.size()) { // Checks if the rows are within the bounds of the matrix
         std::swap(data[r1], data[r2]); // Swaps the two rows
     }
 }
 
 // Swaps two columns in a matrix
-void Matrix::swap_cols(std::size_t c1, std::size_t c2) {
+template <typename T>
+void Matrix<T>::swap_cols(std::size_t c1, std::size_t c2) {
     if (c1 < data.size() && c2 < data.size()) { // Checks if the columns are within the bounds of the matrix
         for (std::size_t i = 0; i < data.size(); i++) { // Iterates over each row in the matrix
             std::swap(data[i][c1], data[i][c2]); // Swaps the two column values in the ith row
@@ -159,7 +166,8 @@ void Matrix::swap_cols(std::size_t c1, std::size_t c2) {
 }
 
 // Prints the matrix
-void Matrix::print_matrix() const {
+template <typename T>
+void Matrix<T>::print_matrix() const {
     std::size_t N = data.size(); // Gets the size of the matrices we are working with
 
     for (std::size_t i = 0; i <  N; i++) { // Iterates over each row in the matrix
@@ -169,3 +177,6 @@ void Matrix::print_matrix() const {
         std::cout << std::endl; // Prints a newline for each new row
     }
 }
+
+template class Matrix<int>; // Explicitly instantiate the template for type int
+template class Matrix<double>; // Explicitly instantiate the template for type double
